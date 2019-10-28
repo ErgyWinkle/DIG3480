a77e871ce482856ff0ae3a26ee17ff1997e83a19
ErgyWinkle <54729998+ErgyWinkle@users.noreply.github.com>
2019-10-27 23:30:04 -0400

    Add files via upload

---
+++ Scripts/CameraAudioScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAudioScript : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public Text countText;
+
+    public Text winText;
+
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count == 4)
+        {
+            winText.text = "You win! Game Created by Ethan Wood!";
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+        private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
